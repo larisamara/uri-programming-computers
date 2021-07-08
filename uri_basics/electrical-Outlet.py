ef69eb28d6977163d27a500c6fb70f8d5ec255f9
Larissa Samara <larissasamaraxavier@gmail.com>
2021-07-08 13:41:08 -0300

    converter numero em quantidade de moedas e de notas

--- uri_basics/electrical-Outlet.py
+++ uri_basics/electrical-Outlet.py
@@ -1,6 +1,3 @@
-number1 = input()
-number2 = input()
-number3 = input()
-number4 = input()
+number1, number2, number3, number4 = input().split()
 result = (int(number1) + int(number2) + int(number3) + int(number4))-3
 print(result)
