503ae713dbb0c7cdd17cb6094083ad8d780bbbe0
Larissa Samara <larissasamaraxavier@gmail.com>
2021-07-09 20:26:12 -0300

    pedagio

--- uri_basics/pedagio.py
+++ uri_basics/pedagio.py
@@ -4,4 +4,4 @@ spent_kilometer_total = int(cost_kilometer) * int(length)
 qtd_toll = int(length) / int(distance)
 spent_toll = int(qtd_toll) * int(cost_toll)
 result = spent_kilometer_total + spent_toll
-print(result)
+print(result) 
