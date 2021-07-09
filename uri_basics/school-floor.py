f666499526a5b7adef4a6cd9265ae2eb96bcdd17
Larissa Samara <larissasamaraxavier@gmail.com>
2021-07-09 19:55:16 -0300

    chao da escola

---
+++ uri_basics/school-floor.py
@@ -0,0 +1,10 @@
+altura = int(input())
+largura = int(input())
+inteiro1 = largura * altura
+inteiro2 = (largura -1) * (altura - 1)
+inteiro = inteiro1 + inteiro2
+metade1 = (altura + altura) - 2
+metade2 = (largura + largura) - 2
+metade = metade1 + metade2
+print(inteiro)
+print(metade)
