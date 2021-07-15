1987bd434fb998b496d45de0329d90bbe39c7404
Larissa Samara <larissasamaraxavier@gmail.com>
2021-07-14 21:41:31 -0300

    maquina de café

--- ur-desvio-de-fluxo/Máquina-de-café.py
+++ ur-desvio-de-fluxo/Máquina-de-café.py
@@ -1,12 +1,12 @@
 a = int(input())
 b = int(input())
 c = int(input())
-andar1 = (b + c) * 2
-andar2 = (a + c) * 2
-andar3 = (a + b) * 2
-if andar1 < andar2:
+andar1 = 2 * b + 4 * c
+andar2 = 2 * a + 2 * c
+andar3 = 2 * b + 4 * a
+if andar1 <= andar2 and andar1 <= andar3:
     print(int(andar1))
-elif andar2 < andar1:
+elif andar2 <= andar1 and andar2 <= andar3:
     print(int(andar2))
 else: 
     print(int(andar3))
