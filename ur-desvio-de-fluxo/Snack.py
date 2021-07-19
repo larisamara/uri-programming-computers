9c0bfe4fd5677ddfa258e39b578b83463135b4e9
Larissa Samara <larissasamaraxavier@gmail.com>
2021-07-19 20:14:34 -0300

    lanche

---
+++ ur-desvio-de-fluxo/Snack.py
@@ -0,0 +1,16 @@
+cod, qtd = input().split()
+if int(cod) == 1:
+   result = int(qtd) * 4
+   print("Total: R$","%.2f" % result)
+if int(cod) == 2:
+   result = int(qtd) * 4.5
+   print("Total: R$","%.2f" % result)
+if int(cod) == 3:
+   result = int(qtd) * 5
+   print("Total: R$","%.2f" % result)   
+if int(cod) == 4:
+   result = int(qtd) * 2
+   print("Total: R$","%.2f" % result)
+if int(cod) == 5:
+   result = int(qtd) * 1.5
+   print("Total: R$","%.2f" % result)
