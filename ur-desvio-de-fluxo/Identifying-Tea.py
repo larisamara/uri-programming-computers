8fb6f26637b698e372783a20c6784b72b657e5a7
Larissa Samara <larissasamaraxavier@gmail.com>
2021-07-20 07:48:56 -0300

    numero de acertos

---
+++ ur-desvio-de-fluxo/Identifying-Tea.py
@@ -0,0 +1,14 @@
+v = int(input())
+a, b, c, d, e = input().split()
+qtd = 0
+if (v == int(a)): 
+ qtd = qtd + 1
+if (v == int(b)):
+ qtd = qtd + 1    
+if (v == int(c)):
+ qtd = qtd + 1 
+if (v == int(d)):
+ qtd = qtd + 1 
+if (v == int(e)):
+ qtd = qtd + 1 
+print(qtd) 
