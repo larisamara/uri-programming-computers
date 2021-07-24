081ef0445a7da5c9860fc23e5ce02094d1dc1c3e
Larissa Samara <larissasamaraxavier@gmail.com>
2021-07-23 21:30:20 -0300

    er

--- ur-desvio-de-fluxo/Triangulos.py
+++ ur-desvio-de-fluxo/Triangulos.py
@@ -7,6 +7,3 @@ elif int(a) ** 2 > (int(b) ** 2) + (int(c) ** 2):
     print("o")
 else:
     print("n")
-Triângulo acutângulo: A² < B² + C²
-Triângulo retângulo: A² = B² + C²
-Triângulo obtusângulo: A² > B² + C²
