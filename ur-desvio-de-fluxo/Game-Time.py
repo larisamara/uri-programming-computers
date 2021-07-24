8d0f482beb8df7e9d5b32e3935cf23cb042b1be1
Larissa Samara <larissasamaraxavier@gmail.com>
2021-07-23 21:40:27 -0300

    jogo time

--- ur-desvio-de-fluxo/Game-Time.py
+++ ur-desvio-de-fluxo/Game-Time.py
@@ -1,10 +1,8 @@
 start, end = input().split()
 result = 0
-if (int(start) < int(end)):
-    result = int(start) - int(end)
-    print("O JOGO DUROU",result,"HORA(S)") 
-elif (int(start) > int(end)):
-    result = (int(end) + 24) - int(start)
-    print("O JOGO DUROU",result,"HORA(S)")
+if(int(start) < int(end)):
+    result = int(end) - int(start)
+    print("O JOGO DUROU", result, "HORA(S)") 
 else:
-    print("O JOGO DUROU 24 HORA(S)")
+    result = int(end) + 24 - int(start)
+    print("O JOGO DUROU", result, "HORA(S)")
